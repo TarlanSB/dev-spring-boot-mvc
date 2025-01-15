021b6524f5a36e21def8451f9cffdaedaa2a6202
TarlanSB <tarlanworkmail@gmail.com>
2025-01-15 10:04:24 +0300

    custom validation

--- src/main/java/com/luv2code/springboot/thymeleafdemo/model/customer/Customer.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/model/customer/Customer.java
@@ -1,22 +1,34 @@
 package com.luv2code.springboot.thymeleafdemo.model.customer;
 
+import com.luv2code.springboot.thymeleafdemo.validation.CourseCode;
 import jakarta.validation.constraints.*;
 
 public class Customer {
 
     private String firstName;
 
-    @NotNull(message="is required")
-    @Size(min=1, message="is required")
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
     private String lastName = "";
 
-    @NotNull(message="is required")
-    @Min(value=0, message="must be greater than or equal to zero")
-    @Max(value=10, message="must be less than or equal to 10")
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to 10")
     private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
+
+    @CourseCode(value = "TOPS", message = "must start with TOPS")
+    private String courseCode;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 
     public String getPostalCode() {
         return postalCode;
